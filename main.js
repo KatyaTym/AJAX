a727f8f80875c64321aa9f2527ab26277e70b642
KatyaTym <palengakatusa01@gmail.com>
2019-08-04 20:31:18 +0300

    Refactoring

--- main.js
+++ main.js
@@ -1,41 +1,52 @@
-const API = 'https://test-users-api.herokuapp.com/';  
+const API = 'https://test-users-api.herokuapp.com/';
 
-let list;
+const list = createUsersList();
 
-function addUserToList(name, age, id) { 
+getUsers();
+createAddBtnOnClickEvent();
+
+function addUserToList(user) {
+    const name = user.name;
+    const age = user.age;
+    const id = user.id;
+
     const element = document.createElement("li");
 
-    console.log(name + " " + age);
+    console.log(name + " " + age + " " + id);
     element.textContent = name + " " + age;
     list.append(element);
 
     const deleteBtn = document.createElement("button");
     deleteBtn.type = "button";
     deleteBtn.onclick = function() {
-    	deleteUser(id);
-    	element.parentNode.removeChild(element);
-    	deleteBtn.parentNode.removeChild(deleteBtn);
+        deleteUser(id);
+        element.parentNode.removeChild(element);
+        deleteBtn.parentNode.removeChild(deleteBtn);
     };
     deleteBtn.textContent = "Delete";
     list.appendChild(deleteBtn);
 }
 
-function createUsersList(users) {
-	let div = document.createElement("div");
-    list= document.createElement("ul");
-    users.data.forEach((user) => {
-        addUserToList(user.name, user.age, user.id);
-    });
+function addUsersToList(users) {
+    users.data.forEach(addUserToList);
+}
+
+function createUsersList() {
+    const div = document.createElement("div");
+    const list = document.createElement("ul");
     div.append(list);
     document.body.append(div);
+    return list;
+};
+
+function getJson(res) {
+    return res.json();
 };
 
 function getUsers() {
-    fetch(API + 'users').then(res => {
-        return res.json();
-    }).then(users => {
-        createUsersList(users);
-    });
+    let response = fetch(API + 'users');
+    let response_json = response.then(getJson);
+    response_json.then(addUsersToList);
 }
 
 function addUser(name, age) {
@@ -43,36 +54,42 @@ function addUser(name, age) {
         name: name,
         age: age
     };
-    fetch(API + 'users', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        }).then(res => {
-            return res.json();
-        })
-        .then(users => {
-            console.log(users);
-            addUserToList(users.data.name, users.data.age);
-        });
+
+    const request_options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    };
+
+    const add_user_data_to_list = function(user) {
+        const u = {
+            name: user.data.name,
+            age: user.data.age,
+            id: user.data._id
+        };
+        addUserToList(u);
+    }
+
+    fetch(API + 'users', request_options).then(getJson).then(add_user_data_to_list);
 }
 
 function deleteUser(id) {
-    fetch(API + 'users/' + id, {
+    const request_options = {
         method: 'DELETE'
-    });
+    };
+
+    fetch(API + 'users/' + id, request_options);
 }
 
 function createAddBtnOnClickEvent() {
-	const addBtn = document.getElementById("addBtn");
-	const name = document.getElementById("name");
-	const age = document.getElementById("age");
-	addBtn.onclick = function() {
-		//console.log(name.value + " " + age.value);
-		addUser(name.value, age.value);
-	};
+    const addBtn = document.getElementById("addBtn");
+    const name = document.getElementById("name");
+    const age = document.getElementById("age");
+    addBtn.onclick = function() {
+        addUser(name.value, age.value);
+        name.value = "";
+        age.value = "";
+    };
 }
-
-getUsers();
-createAddBtnOnClickEvent();
